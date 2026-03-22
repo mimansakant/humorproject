b487ac535db2925ef3d290a1d434268012a839a7
Mimansa <mk4845@barnard.edu>
2026-03-22 18:56:29 -0400

    Revert "Add images grid page with Supabase connection"
    This reverts commit f509d042491f7ad34e3ba85adcd54c5f83af0284.

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = `https://${process.env.SUPABASE_PROJECT_ID}.supabase.co`
-const supabaseKey = process.env.SUPABASE_ANON_KEY!
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.supabase.co',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
